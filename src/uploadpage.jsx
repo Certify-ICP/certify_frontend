70ad40f4d85c01ada74c374cf5a94b8759446fc0
Kalaro Teniola Vienna <teniolakalaro@gmail.com>
2024-02-05 15:49:41 +0100

    fixed footer on upload page

--- src/uploadpage.jsx
+++ src/uploadpage.jsx
@@ -1,25 +1,20 @@
 import { useState, React } from "react";
-import { FileUploader } from "react-drag-drop-files";
 import Nav from "./navbar";
 import Footer from "./footer";
 
 function Upload() {
   const fileTypes = ["PDF", "JPG", "PNG"];
-  const [firstname, setFirstName] =useState('')
-  const [lastame, setLastName] =useState('')
-  const [institution, setInstitution] =useState('')
+  const [firstname, setFirstName] = useState("");
+  const [lastame, setLastName] = useState("");
+  const [institution, setInstitution] = useState("");
 
-  const [file, setFile] = useState(null);
-  const handleChange = (file) => {
-    setFile(file);
-  };
+  
 
   return (
-    <div className=" absolute inset-0 h-screen font-['Rubik'] bg-[#012a4a]">
+    <div className="flex flex-col inset-0 h-screen font-['Rubik'] bg-[#012a4a]">
       <Nav />
-      <div className=" flex flex-col relative h-[500px] items-center w-screen ">
-        <div className="bg-white h-[300px] w-screen "></div>
-        <div className=" absolute bottom-0 bg-[#ffff] p-2 h-fit w-fit rounded-lg shadow-lg ">
+      <div className="flex-grow mb-10 flex flex-col justify-center items-center w-screen ">
+        <div className="p-2 h-fit bg-white w-fit rounded-lg shadow-lg ">
           <h4 className="text-[26px] text-[#012a4a] font-bold p-2">
             Upload your certificate
           </h4>
@@ -28,14 +23,17 @@ function Upload() {
             className="flex flex-col justify-center items-center p-2 "
           >
             <span className="flex">
-              <input
-                type="text"
-                name="first"
-                id=""
-                placeholder="First Name"
-                className="w-[400px] h-[40px] m-2 rounded-lg p-2 border border-[#415a77] focus:ring-blue-500 focus:border-blue-500  "
-                required
-              />
+              <div>
+                <label htmlFor="#first-name"></label>
+                <input
+                  type="text"
+                  name="first"
+                  id="first-name"
+                  placeholder="First Name"
+                  className="w-[400px] h-[40px] m-2 rounded-lg p-2 border border-[#415a77] focus:ring-blue-500 focus:border-blue-500  "
+                  required
+                />
+              </div>
 
               <input
                 type="text"
@@ -58,15 +56,15 @@ function Upload() {
               <option value="Covenant">Covenant University</option>
             </select>
             <span className="flex flex-col">
-            <label htmlFor="file" className="text-[#012a4a] mt-4 ">
-              Attach Your Certificate File Here!
-            </label>
-            <input
-              type="file"
-              name="file"
-              id=""
-              required
-              className=" block w-[350px] text-sm text-gray-500 my-2 mb-4
+              <label htmlFor="file" className="text-[#012a4a] mt-4 ">
+                Attach Your Certificate File Here!
+              </label>
+              <input
+                type="file"
+                name="file"
+                id=""
+                required
+                className=" block w-[350px] text-sm text-gray-500 my-2 mb-4
               file:me-4 file:py-2 file:px-4
               file:rounded-lg file:border-0
               file:text-sm file:font-semibold
@@ -74,7 +72,8 @@ function Upload() {
               hover:file:bg-[#012a4ab7]
               file:disabled:opacity-50 file:disabled:pointer-events-none
              "
-            /></span>
+              />
+            </span>
             {/* <FileUploader
               handleChange={handleChange}
               name="file"
@@ -88,7 +87,6 @@ function Upload() {
               className=" w-[300px] h-[50px] text-[18px] my-2 py-2 px-4 bg-[#012a4a] text-white rounded-lg"
             />
           </form>
-          
         </div>
       </div>
       <Footer />
