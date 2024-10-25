c6d20e4235edb8c89d1c75e64f26aa9c3ee87a79
Kalaro Teniola Vienna <teniolakalaro@gmail.com>
2024-10-25 16:42:55 +0100

    Updates to landing page

--- src/landingpage.jsx
+++ src/landingpage.jsx
@@ -14,7 +14,7 @@ function Home() {
     {
       id: 1,
       icon: (
-        <CheckIcon className="  bg-[#778da91a] w-[60px] h-[60px] rounded-full p-2 text-[#61a5c2] " />
+        <CheckIcon className="  bg-[#778da91a] w-[55px] h-[55px] rounded-full p-3 text-[#61a5c2] " />
       ),
       title: "Instant Verification",
       content: "Instantly verify the authenticity of certificates, ",
@@ -22,7 +22,7 @@ function Home() {
     {
       id: 2,
       icon: (
-        <GlobeIcon className="  bg-[#778da91a] w-[60px] h-[60px] rounded-full p-2 text-[#61a5c2] " />
+        <GlobeIcon className="  bg-[#778da91a] w-[55px] h-[55px] rounded-full p-3 text-[#61a5c2] " />
       ),
       title: "Global Accessibility",
       content: "Accessible anywhere in the world.",
@@ -30,7 +30,7 @@ function Home() {
     {
       id: 3,
       icon: (
-        <LockClosedIcon className="  bg-[#778da91a] w-[60px] h-[60px] rounded-full p-2 text-[#61a5c2]" />
+        <LockClosedIcon className="  bg-[#778da91a] w-[55px] h-[55px] rounded-full p-3 text-[#61a5c2]" />
       ),
       title: "Data Security",
       content: "We protect your data from unwanted access",
@@ -39,7 +39,7 @@ function Home() {
     {
       id: 4,
       icon: (
-        <CheckIcon className="  bg-[#778da91a] w-[60px] h-[60px] rounded-full p-2 text-[#61a5c2]" />
+        <CheckIcon className="  bg-[#778da91a] w-[55px] h-[55px] rounded-full p-3 text-[#61a5c2]" />
       ),
       title: "Easy",
       content: "We ensure an easy process to verifying these certificates",
@@ -49,16 +49,21 @@ function Home() {
   const works = [
     {
       id: 1,
-      content: ["Upload your certificate ", <Link to="/upload">here</Link>],
+      content: [
+        "Upload your certificate ",
+        <Link to="/upload" className=" hover:text-[#ffffffb1]">
+          here.
+        </Link>,
+      ],
     },
-    { id: 2, content: "We verify the certificate " },
+    { id: 2, content: "We verify the certificate. " },
     {
       id: 3,
-      content: "BLAH BLAH ",
+      content: "You get issued an id. ",
     },
     {
       id: 4,
-      content: "yoyoyoupufvf",
+      content: "Share your issued id with anyone.",
     },
     {
       id: 5,
@@ -80,7 +85,7 @@ function Home() {
 
   const Items = works.map((work) => (
     <div className="flex m-6 items-center w-[380px]">
-      <div className="bg-white justify-self-start  text-[20px] text-[#0d1b2a] w-12 h-12 rounded-full flex justify-center items-center">
+      <div className="bg-white justify-self-start  text-[17px] text-[#0d1b2a] w-[45px] h-[45px] p-1 rounded-full flex justify-center items-center">
         {work.id}
       </div>
       <div className=" mx-4 text-[20px] w- text-left  ">{work.content}</div>
@@ -97,7 +102,7 @@ function Home() {
           </h2>
           <a href="#about">
             <p className="p-2  w-[500px] text-[#415a77] text-center">
-              Unlock the Power of Verified Credentials with Blockchain
+              Unlocking the Power of Verified Credentials with Blockchain
               Technology!
             </p>
           </a>
@@ -108,24 +113,27 @@ function Home() {
       </div>
       <section
         id="about"
-        className=" flex justify-between items-center h-[600px] bg-white mt-[300px] text-[#415a77] p-4 "
+        className=" flex justify-between items-center h-[600px] bg-white mt-[170px] text-[#415a77] py-[300px] border-y-[50px] border-[#012a4a] mb-0"
       >
-        <div>
-          <h1 className=" text-[60px] font-extrabold w-[600px] text-[#0d1b2a] ml-20 ">
-            Our goal is to help verify and secure the authenticity of
-            certificates.
-          </h1>
+        {" "}
+        <div className=" flex justify-between items-center">
+          <div>
+            <h1 className=" text-[55px] font-extrabold w-[600px] text-[#0d1b2a] ml-20 ">
+              Our goal is to help verify and secure the authenticity of
+              certificates.
+            </h1>
+          </div>
+          <div className=" grid grid-cols-2 gap-2 grid-rows-2 ml-0">{Item}</div>{" "}
         </div>
-        <div className=" grid grid-cols-2 gap-2 grid-rows-2 ml-0">{Item}</div>
       </section>
       <section
         id="howitworks"
-        className=" bg-[#012a4a] h-fit p-5 flex flex-col justify-center items-center text-white"
+        className=" border-y-[60px] border-[#012a4a]  bg-[#012a4a] h-fit p-5 flex flex-col justify-center items-center text-white mt-0"
       >
-        <h3 className="mt-[80px] font-extrabold text-[50px] w-[600px] text-center text-white leading-tight">
+        <h3 className="mt-[0px] mb-[20px] font-extrabold text-[50px] w-[600px] text-center text-white leading-tight">
           How it works
         </h3>
-        <div>{Items}</div>
+        <div className=" mb-[30px]">{Items}</div>
       </section>
       <Footer />
       <Outlet />
